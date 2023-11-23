0db45409df3b789706f3b6ee27d4268400045109
Robert <robertbroersma@gmail.com>
2023-11-23 14:57:41 +0100

    poke

--- App/dev/stac/[...].ts
+++ App/dev/stac/[...].ts
@@ -3,7 +3,7 @@ import fs from 'node:fs/promises'
 export default defineEventHandler(async (event) => {
   let path = event.context.params?.['_']
 
-  if (!path) throw new Error('No path provided.')
+  if (!path) throw new Error('No path provided')
 
   let json = await fs.readFile(`${process.cwd()}/../STAC/data/current/${path}`)
 
