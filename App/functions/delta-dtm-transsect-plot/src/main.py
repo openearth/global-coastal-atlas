e28a4453158aa183fed9cfc6bf0be6df07d228b2
Nathan Clerkx <nathan@wolk.work>
2025-03-07 14:49:18 +0100

    dont watch for changes

--- App/functions/delta-dtm-transsect-plot/src/main.py
+++ App/functions/delta-dtm-transsect-plot/src/main.py
@@ -127,4 +127,4 @@ async def root():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=settings.port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=settings.port)
